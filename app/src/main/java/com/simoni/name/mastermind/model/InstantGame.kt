7f4dc4ffaf1eb8b011da5ce10fd40edda60532eb
Leoanrdo Simoni <leonardo.simoni.01@students.uniroma2.eu>
2023-08-13 22:32:22 +0200

    Prima implementazione della classe InstantGame con il metodo "attempt" ch e gestisce totto lo stato del gioco più o meno

--- app/src/main/java/com/simoni/name/mastermind/model/InstantGame.kt
+++ app/src/main/java/com/simoni/name/mastermind/model/InstantGame.kt
@@ -1,15 +1,18 @@
 package com.simoni.name.mastermind.model
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import java.util.Calendar
 
+
 class InstantGame() {
     var secret = mutableStateOf("")
-    var attempts = mutableStateOf("")
-    var duration = mutableStateOf(0.0)
+    var attempts = mutableStateListOf("")
+    var duration = mutableStateOf(System.currentTimeMillis())
     var date = mutableStateOf("")
-
-    val color = listOf("W","R","C","G","Y","P","O","B")
+    var status = mutableStateOf("")
+    var life = mutableStateOf(0)
+    val color = listOf("W", "R", "C", "G", "Y", "P", "O", "B")
     /*
     W -> white
     R -> red
@@ -22,25 +25,77 @@ class InstantGame() {
      */
 
 
-    fun newMatch(){
-        secret.value = color.random() + color.random() + color.random() + color.random() + color.random()
-        attempts.value = ""
-        duration.value = 0.0
+    fun newMatch() {
+        secret.value =
+            color.random() + color.random() + color.random() + color.random() + color.random()
+        attempts.clear()
+        duration.value = System.currentTimeMillis()
+        duration.value.javaClass.kotlin.simpleName
         date.value = Calendar.getInstance().get(Calendar.DAY_OF_MONTH).toString() + "/" +
-                "" + (Calendar.getInstance().get(Calendar.MONTH)+1).toString() + "/" +
+                "" + (Calendar.getInstance().get(Calendar.MONTH) + 1).toString() + "/" +
                 "" + Calendar.getInstance().get(Calendar.YEAR).toString()
+        status.value = "ongoing"
+        life.value = 10
     }
 
-    fun attempt(){
-        //TODO
+
+    fun attempt(guess: String) {
+        var ngg: Int = 0
+        var ngs: Int = 0
+        var newSecret = ""
+        var newGuess = ""
+        val evaluatedChars = mutableListOf<Char>()
+
+        // Numero di cifre giuste al posto giusto
+        for(i in 0 until secret.value.length)
+            if(secret.value[i] == guess[i])
+                ngg ++
+
+        // Numero di cifre giuste al posto sbagliato
+        for (i in 0 until secret.value.length) {
+            if (secret.value[i] != guess[i]) {
+                newSecret += secret.value[i]
+                newGuess += guess[i]
+            }
+        }
+
+        if (!newSecret.isEmpty()) {
+            for (letter in guess) {
+                if (!evaluatedChars.contains(letter)) {
+                    val howManyInSecret = countHowMany(newSecret, letter)
+                    val howManyInGuess = countHowMany(newGuess, letter)
+
+                    ngs += if (howManyInSecret == howManyInGuess || howManyInSecret > howManyInGuess) howManyInGuess
+                    else howManyInSecret
+
+                    evaluatedChars.add(letter)
+                }
+            }
+        }
+
+        attempts.add("${guess}/${ngg}/${ngs}")
+        life.value -= 1
+
+        if (ngg == secret.value.length)
+            status.value = "win"
+        else if (life.value < 1)
+            status.value = "lose"
     }
 
-    fun saveOnDb() {
-
+    private fun countHowMany(letters: String, letter: Char): Int {
+        var howMany = 0
+        for (i in 0 until letters.length) {
+            if (letters[i] == letter) {
+                howMany++
+            }
+        }
+        return howMany
     }
+
+
+
 
     fun loadMatch() {
-
+        //TODO se ci va
     }
-
 }
